17846d4bd68aaba987918f1520ac67ec38614859
Nick McClure <nfmcclure@gmail.com>
2016-02-23 15:43:07 -0800

    Added R file

--- 8_NLP/eager_vs_lazy.R
+++ 8_NLP/eager_vs_lazy.R
@@ -25,21 +25,52 @@ setwd('/home/nick/Documents/teaching/DataScience350/8_NLP/')
 # You can imagine R has both ways to do this.
 
 ##-----Eager evaluation-------
-if (1==2 & 5==NA){
+if (1==2 & 5==NULL){
   print("True!")
 } else{
   print("False!")
 }
 
-if ((1==1) | (5==NA)){
+if ((1==1) | (5==NULL)){
   print("True!")
 } else{
   print("False!")
 }
 
 ##-----Lazy Evaluation-------
-if ((1==2) && (5==NA)){
+if ((1==2) && (5==NULL)){
   print("True!")
 } else{
   print("False!")
 }
+
+if ((1==1) || (5==NULL)){
+  print("True!")
+} else{
+  print("False!")
+}
+
+
+# What are the implications of this?
+test_matrix = matrix(rnorm(1000000), nrow = 1000)
+
+# eager:
+system.time(apply(test_matrix,1,function(r) sapply(r,function(c) {
+  if(c>3 & c<3.1){c}else{0}
+  })))
+
+# lazy:
+system.time(apply(test_matrix,1,function(r) sapply(r,function(c) {
+  if(c>3 && c<3.1){c}else{0}
+})))
+
+# Even harder:
+# eager:
+system.time(apply(test_matrix,1,function(r) sapply(r,function(c) {
+  if(c>3 & abs(c**2+3*c-2)<0.999 & sin(tan(abs(pi*c)))>0.5){c}else{0}
+  })))
+
+# lazy:
+system.time(apply(test_matrix,1,function(r) sapply(r,function(c) {
+  if(c>3 && abs(c**2+3*c-2)<0.999 && sin(tan(abs(pi*c)))>0.5){c}else{0}
+})))
